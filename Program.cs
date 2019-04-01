b24c265eccc0d48117e25115f12b5ce9ab1bb47f
Orcun Cakiroglu <S01073570@acad.tri-c.edu>
2019-03-31 19:33:18 -0600

    Homework 6 fully completed.

--- Program.cs
+++ Program.cs
@@ -10,6 +10,37 @@ namespace ConsoleTempConverter
     {
         static void Main(string[] args)
         {
+            string userInput;
+            string userChoice;
+            string userValue;
+            double userConverted;
+            double result;
+            do
+            {
+                Console.WriteLine("Would you like to do tempretature convertion? Type 'Y' to continue or 'Exit' to close the program");
+                userInput = Console.ReadLine();
+                if(userInput == "Y" || userInput =="y")
+                {
+                    Console.WriteLine(" Fahrenheit to Celsius  (Type '1') or  Celsius to Fahrenheit (Type '2') ");
+                    userChoice = Console.ReadLine();
+                    Console.WriteLine("Please enter the temprature value:");
+                    userValue = Console.ReadLine();
+
+                    if (userChoice == "1")
+                    {
+                        userConverted = Convert.ToDouble(userValue);
+                        result = (userConverted - 32) * 5 / 9;
+                        Console.WriteLine( userConverted + " Fahrenheit is equal " + result + " Celsius");
+                    }
+                    else if( userChoice == "2")
+                    {
+                        userConverted = Convert.ToDouble(userValue);
+                        result = (userConverted * 9 / 5) + 32;
+                        Console.WriteLine(userConverted + " Celsius is equal " + result + " Fahrenheit");
+                    }
+                }
+        
+            } while (userInput != "Exit");
         }
     }
 }
